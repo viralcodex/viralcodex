080e8c1f34e3cdcf1682e610dc39493ff819497a
Aviral Shukla <79330197+viralcodex@users.noreply.github.com>
2025-10-10 19:00:19 +0530

    Update update-contributions.js

--- update-contributions.js
+++ update-contributions.js
@@ -68,7 +68,7 @@ function generateTable(prs) {
   const rows = prs
     .map(
       (pr) =>
-        `| [${pr.repo}](https://github.com/${pr.repo}) | [${pr.title}](${pr.url}) | ${pr.state === "open" ? "🟢Open" : "🔵Merged"} |`
+        `| [${pr.repo}](https://github.com/${pr.repo}) | [${pr.title}](${pr.url}) | ${pr.state === "open" ? "`Open`" : "`Merged`"} |`
     ).join("\n");
 
   return `${header}\n${rows}`;
